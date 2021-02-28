7b21c22ac4d9c70eb55f03bd10d831501b75d4db
Laroosevelt21 <laroosevelt21@gmail.com>
2021-02-28 13:11:34 -0500

    Completion of Assignment 4.

--- analyze.py
+++ analyze.py
@@ -2,11 +2,19 @@ import numpy
 import os
 import matplotlib.pyplot
 
-backLegSensorValues = numpy.load(os.path.join('data', 'backLegSensorValues.npy'))
+##backLegSensorValues = numpy.load(os.path.join('data', 'backLegSensorValues.npy'))
 ##print(backLegSensorValues)
-frontLegSensorValues = numpy.load(os.path.join('data', 'frontLegSensorValues.npy'))
+##frontLegSensorValues = numpy.load(os.path.join('data', 'frontLegSensorValues.npy'))
 
-matplotlib.pyplot.plot(backLegSensorValues,linewidth=3)
-matplotlib.pyplot.plot(frontLegSensorValues)
-matplotlib.pyplot.legend()
+backLegTargetAngles = numpy.load(os.path.join('data', 'backLegTargetAngles.npy'))
+frontLegTargetAngles = numpy.load(os.path.join('data', 'frontLegTargetAngles.npy'))
+
+##matplotlib.pyplot.plot(backLegSensorValues,linewidth=3)
+##matplotlib.pyplot.plot(frontLegSensorValues)
+##matplotlib.pyplot.legend()
+##matplotlib.pyplot.show()
+
+matplotlib.pyplot.plot(backLegTargetAngles, linewidth=5, label = 'Back Leg Motor Values')
+matplotlib.pyplot.plot(frontLegTargetAngles, linewidth=2, label = 'Front Leg Motor Values')
+matplotlib.pyplot.legend(loc = 'upper left')
 matplotlib.pyplot.show()
