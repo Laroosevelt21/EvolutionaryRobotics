913d1717671c253c78edca9257b47a1b149f17b5
Laroosevelt21 <laroosevelt21@gmail.com>
2021-03-12 11:56:57 -0500

    Completion of Assignment 6:neurons.

--- robot.py
+++ robot.py
@@ -3,6 +3,7 @@ from motor import MOTOR
 import pyrosim.pyrosim as pyrosim
 import pybullet as p
 import pybullet_data
+from pyrosim.neuralNetwork import NEURAL_NETWORK
 
 class ROBOT:
 
@@ -11,6 +12,7 @@ class ROBOT:
         pyrosim.Prepare_To_Simulate("body.urdf")
         self.Prepare_To_Sense()
         self.Prepare_To_Act()
+        self.nn = NEURAL_NETWORK("brain.nndf")
 
     def Prepare_To_Sense(self):
         self.sensors = {}
@@ -27,5 +29,14 @@ class ROBOT:
             self.motors[jointName] = MOTOR(jointName)
             
     def Act(self, timeStep):
-        for j in self.motors:
-            self.motors[j].Set_Value(self.robot, timeStep)
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                self.motors[jointName].Set_Value(self.robot, desiredAngle)
+
+
+    def Think(self):
+        self.nn.Update()
+        self.nn.Print()
+    
--- simulation.py
+++ simulation.py
@@ -16,6 +16,7 @@ class SIMULATION:
         for t in range(c.timeSteps):
             p.stepSimulation()
             self.robot.Sense(t)
+            self.robot.Think()
             self.robot.Act(t)
             time.sleep(c.sleepTime)
     
